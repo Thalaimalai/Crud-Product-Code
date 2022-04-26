51d67ba9f55a0314dd3d42748b0e7ffb4cf36962
Thalaimalai <98823043+Thalaimalai@users.noreply.github.com>
2022-04-26 11:24:17 +0530

    Update StudentErrorCodes.java

--- studentrecord/src/main/java/com/tlc/crm/studentrecord/status/StudentErrorCodes.java
+++ studentrecord/src/main/java/com/tlc/crm/studentrecord/status/StudentErrorCodes.java
@@ -3,6 +3,11 @@ package com.tlc.crm.studentrecord.status;
 import com.tlc.commons.code.ErrorCodeGroup;
 import com.tlc.commons.code.ErrorCodeProvider;
 
+/**
+ * Provides the custom error codes.
+ *
+ * @author ThalaimalaiPandiyanT
+ */
 public enum StudentErrorCodes implements ErrorCodeProvider {
 
     VALIDATION_FAILED(0x01),
@@ -20,6 +25,9 @@ public enum StudentErrorCodes implements ErrorCodeProvider {
         return code;
     }
 
+    /**
+     * Class to get the instance of student error codes.
+     */
     private static class StudentErrorCodeGroup implements ErrorCodeGroup {
 
         private static final ErrorCodeGroup GROUP = new StudentErrorCodes.StudentErrorCodeGroup();
